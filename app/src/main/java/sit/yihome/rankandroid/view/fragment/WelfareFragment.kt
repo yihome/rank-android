c09b9b5f754244a4c0cbd7affdf7453b2770f29d
houyi <houyif2@outlook.com>
2018-07-30 20:22:56 +0800

    dailyUpdate

--- app/src/main/java/sit/yihome/rankandroid/view/fragment/WelfareFragment.kt
+++ app/src/main/java/sit/yihome/rankandroid/view/fragment/WelfareFragment.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.StaggeredGridLayoutManager
--- app/src/main/java/sit/yihome/rankandroid/view/helper/WelFareItemDecoration.kt
+++ app/src/main/java/sit/yihome/rankandroid/view/helper/WelFareItemDecoration.kt
@@ -1,9 +1,12 @@
 package sit.yihome.rankandroid.view.helper
 
+import android.graphics.Rect
 import android.support.v7.widget.RecyclerView
+import android.view.View
 
 class WelFareItemDecoration : RecyclerView.ItemDecoration() {
 
-
-
+    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+        super.getItemOffsets(outRect, view, parent, state)
+    }
 }
